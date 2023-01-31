8fc27d52441bae83f1adab171fe24c463336f181
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-31 11:13:19 -0600

    completed assignment 2

--- assignment_2/stringchar_gr.cpp
+++ assignment_2/stringchar_gr.cpp
@@ -1,6 +1,6 @@
 // This program demonstrates the use of characters and strings
 
-// PLACE YOUR NAME HERE
+// Greg Rodriguez
 
 #include <iostream>
 #include <string>
@@ -12,23 +12,19 @@ const char BESTRATING = 'A';				// use single quotes for characters
 
 int main()
 {
-	char rating;			// 2nd highest product rating
-	string favoriteSnack;	// most preferred snack
-	int numberOfPeople;		// the number of people in the survey
-	int topChoiceTotal;		// the number of people who prefer the top choice
-
-	// Fill in the code to do the following:
-	// Assign the value of "crackers" to favoriteSnack
-	// Assign a grade of 'B' to rating
-	// Assign the number 250 to the numberOfPeople
-	// Assign the number 148 to the topChoiceTotal
+	char rating = 'B';					// 2nd highest product rating
+	string favoriteSnack = "crackers";	// most preferred snack
+	int numberOfPeople = 250;			// the number of people in the survey
+	int topChoiceTotal = 148;			// the number of people who prefer the top choice
+	// FAVORITESODA = "Sprite";			// attempt to change variable value
+	favoriteSnack = "cookies";			// attmept to change variable value
 
 	// Fill in the blanks of the following:
-	cout << "The preferred soda is " << << endl; 
-	cout << "The preferred snack is " << << endl; 
-	cout << "Out of " << << " people "
-		 << << " chose these items!" << endl;
-	cout << "Each of these products were given a rating of " << ;
+	cout << "The preferred soda is " << FAVORITESODA << endl; 
+	cout << "The preferred snack is " << favoriteSnack << endl; 
+	cout << "Out of " << numberOfPeople << " people "
+		 << topChoiceTotal << " chose these items!" << endl;
+	cout << "Each of these products were given a rating of " << BESTRATING;
 	cout << " from our expert tasters" << endl;
 	cout << "The other products were rated no higher than a " << rating
 		 << endl;
