c5dece7bd9cab1b93383ab2156c4565a7bd46f05
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-01-30 22:43:43 -0600

    completed circlearea.cpp

--- assignment_2/circlearea_gr.cpp
+++ assignment_2/circlearea_gr.cpp
@@ -1,9 +1,10 @@
 // This program will output the circumference and area
 // of the circle with a given radius.
 
-// PLACE YOUR NAME HERE
+// Greg Rodriguez
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 const double PI = 3.14;
@@ -11,16 +12,16 @@ const double RADIUS = 5.4;
 
 int main()
 {
-	area								// definition of area of circle 
+	float area;							// definition of area of circle 
 	float circumference;				// definition of circumference 
 	circumference = 2 * PI * RADIUS;	// computes circumference
-	area = ;							// computes area
+	area = PI * pow(RADIUS, 2);			// computes area
 
 	// Fill in the code for the cout statement that will output (with description)
 	// the circumference
-
+	cout << "The circumference of a circle with a radius of " << RADIUS << " is " << circumference << "\n";
 	// Fill in the code for the cout statement that will output (with description)
 	// the area of the circle
-
+	cout << "The area of a circle with a radius of " << RADIUS << " is " << area << "\n";
 	return 0;
 }
