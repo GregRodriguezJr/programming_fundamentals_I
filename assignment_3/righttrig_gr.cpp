19071b1e334d9e79a09835ca2f15533a1a8b00b5
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-07 18:58:08 -0600

    formatted output and added iomainip library for calulations

--- assignment_3/righttrig_gr.cpp
+++ assignment_3/righttrig_gr.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <cmath>	// needed for math functions like sqrt()
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -12,14 +13,16 @@ int main()
 	float a, b;	// the smaller two sides of the triangle 
 	float hyp;	// the hypotenuse calculated by the program
 
-	cout << "Please input the value of the two sides" << endl;
+	cout << "\nPlease input the value of the two sides" << endl;
 	cin >> a >> b;
 
 	// Fill in the assignment statement that determines the hypotenuse
+	hyp = sqrt(pow(a, 2) + pow(b, 2));
 
 	cout << "The sides of the right triangle are " << a << " and " << b << endl;
 
-	cout << "The hypotenuse is " << hyp << endl;
+	cout << setprecision(2) << fixed; // formatted output
+	cout << "The hypotenuse is " << hyp << "\n" << endl;
 
 	return 0;
 }
