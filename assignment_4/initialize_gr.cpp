2aeae1900f3b802fcf98e8530c02036506925932
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-20 22:02:14 -0600

    completed exercises for initilize

--- assignment_4/initialize_gr.cpp
+++ assignment_4/initialize_gr.cpp
@@ -8,19 +8,26 @@ using namespace std;
 
 int main()
 {
-	int num1,	// num1 is not initialized
-	num2 = 5;	// num2 has been initialized to 5
+	int num1, num2;	// num1 and num2 have not been initialized
 
-	cout << "Please enter an integer" << endl;
+	cout << "\nPlease enter an integer" << endl;
 	cin >> num1;
+
+    // exercise 2
+    cout << "\nPlease enter another integer" << endl;
+	cin >> num2;
 
 	cout << "num1 = " << num1 << " and num2 = " << num2 << endl;
 
-	if (num1 = num2)
-		cout << "Hey, that�s a coincidence!" << endl;
-
-	if (num1 != num2)
-		cout << "The values are not the same" << endl;
+    // exercise 1
+	if (num1 == num2) {
+        //exercise 3
+        cout << "The values are the same." << endl;
+        cout << "Hey, that's a coincidence!" << endl;
+      // exercise 4
+    } else {
+        cout << "The values are not the same" << endl;
+    }
 
 	return 0;
 }
