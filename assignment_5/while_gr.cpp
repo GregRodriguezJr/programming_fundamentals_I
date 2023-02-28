f548b935ebef7e66b0ce3c38f9abe82871805a0e
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-02-27 21:58:09 -0600

    completed while loop exercise

--- assignment_5/while_gr.cpp
+++ assignment_5/while_gr.cpp
@@ -1,19 +1,33 @@
-// PLACE YOUR NAME HERE
+// Greg Rodriguez
 
 #include <iostream>
 using namespace std;
 
 int main()
-{
-	char letter = 'a';
+{   // step 2
+//	char letter = 'a';
+//
+//    cout << "\nEnter x to exit" << endl;
+//
+//	while (letter != 'x')
+//	{
+//		cout << "Please enter a letter to continue" << endl;
+//		cin >> letter;
+//
+//		cout << "The letter you entered is " << letter << endl;
+//	}
 
-	while (letter != 'x')
-	{
-		cout << "Please enter a letter" << endl;
+    // step 3
+    char letter;
+
+    cout << "\nEnter x to exit" << endl;
+
+    do {
+        cout << "Please enter a letter to continue" << endl;
 		cin >> letter;
 
 		cout << "The letter you entered is " << letter << endl;
-	}
+    } while (letter != 'x');
 
 	return 0;
 }
