c32e4ba13e082291521347570e739488ec830b3b
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-04 14:56:09 -0600

    completed nested loop exercise

--- assignment_5/nested_gr.cpp
+++ assignment_5/nested_gr.cpp
@@ -1,7 +1,7 @@
 // This program finds the average time spent programming by a student
 // each day over a three day period.
 
-// PLACE YOUR NAME HERE
+// Greg Rodriguez
 
 #include <iostream>
 using namespace std;
@@ -9,33 +9,60 @@ using namespace std;
 int main()
 {
 	int numStudents;
-	float numHours, total, average;
+	float numHours, numHours2, total, average;
 	int student, day = 0;	// these are the counters for the loops
+    int numOfDays;
+    string result;          // string to hold the result of studying
 
 	cout << "This program will find the average number of hours a day"
-		 << " that a student spent programming over a long weekend\n\n";
-	cout << "How many students are there ?" << endl << endl;
+		 << " that a student spent studying over a long weekend\n\n";
+	cout << "How many students are there ?" << endl;
 	cin >> numStudents;
+
+    // Ask user for how many days to enter
+    cout << "How many days would you like to consider?" << endl;
+    cin >> numOfDays;
+
+    // While loop to ensure data is a positive number
+    while (numOfDays <= 0) {
+        cout << "Error - please enter a positive number for amount of days" << endl;
+        cout << "How many days would you like to consider?" << endl;
+        cin >> numOfDays;
+    }
 
 	for (student = 1; student <= numStudents; student++)
 	{
 		total = 0;
 
-		for (day = 1; day <= 3; day++)
+        // Biology
+		for (day = 1; day <= numOfDays; day++)
 		{
-			cout << "Please enter the number of hours worked by student "
-				 << student << " on day " << day << "." << endl;
+			cout << "\nPlease enter the number of hours worked by student "
+				 << student << " on day " << day << " in biology." << endl;
 			cin >> numHours;
 
-			total = total + numHours;
+            cout << "\nPlease enter the number of hours worked by student "
+				 << student << " on day " << day << " in programming." << endl;
+			cin >> numHours2;
+
+			total = total + numHours + numHours2;
 		}
 
-		average = total / 3;
+		average = total / static_cast<float>(numOfDays);
+
+        // Conditional to print results of most time spent on subject
+        if (numHours == numHours2) {
+            result = "neither, same amount of time.";
+        } else if ( numHours < numHours2) {
+            result = "programming.";
+        } else {
+            result = "biology.";
+        }
 
 		cout << endl;
-		cout << "The average number of hours per day spent programming by "
-			 << "student " << student << " is " << average
-			 << endl << endl << endl;
+		cout << "The average number of hours per day spent studying by "
+			 << "student " << student << " is " << average << endl
+             << "The most study time was spent on " << result << endl;
 	}
 
 	return 0;
