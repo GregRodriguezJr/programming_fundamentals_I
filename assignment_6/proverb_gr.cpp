a0daa70fe379583e2d6e9b73009caf9b7ecb1561
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-06 11:52:03 -0600

    completed proverb exercise

--- assignment_6/proverb_gr.cpp
+++ assignment_6/proverb_gr.cpp
@@ -12,7 +12,7 @@ void writeProverb();	// This is the prototype for the writeProverb function
 int main()
 {
 	// Fill in the code to call the writeProverb function
-
+    writeProverb();
 	return 0;
 }
 
@@ -28,3 +28,6 @@ int main()
 // Fill in the function heading and the body of the function that will print
 // to the screen the proverb listed in the comments at the beginning of the
 // program
+void writeProverb() {
+    cout << "\nNow is the time for all good men to come to the aid of their party" << endl;
+}
