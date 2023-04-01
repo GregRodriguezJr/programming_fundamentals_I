f6ff33ac7f240fd41e9563ab068f405addb19bfb
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-03-31 20:49:42 -0500

    completed exercise 7.4 temp program

--- assignment_7/temp_gr.cpp
+++ assignment_7/temp_gr.cpp
@@ -19,18 +19,27 @@ typedef int TempArrType[MAXTEMP];
 
 void setTemp(TempArrType, int&);
 float getAverageTemp(const TempArrType, int);
+int highestTemp(const TempArrType, int);
+int lowestTemp(const TempArrType, int);
 
 int main () {
 
     int arrayLength = 0;
     TempArrType tempArr;
     float tempAverage;
+    int highTemp;
+    int lowTemp;
 
     setTemp(tempArr, arrayLength);
 
     tempAverage = getAverageTemp(tempArr, arrayLength);
     cout << fixed << showpoint << setprecision(2);
     cout << "The average temperature is " << tempAverage << " degrees " << endl;
+
+    highTemp = highestTemp(tempArr, arrayLength);
+    lowTemp = lowestTemp(tempArr, arrayLength);
+    cout << "The highest temp is : " << highTemp << endl;
+    cout << "The lowest temp is : " << lowTemp << endl;
 
     return 0;
 }
@@ -54,4 +63,21 @@ float getAverageTemp(const TempArrType tempArr, int arrLength) {
         sum += tempArr[i];
     }
     return sum / arrLength;
+}
+
+// function returns highest temp
+int highestTemp(const TempArrType tempArr, int arrLength) {
+    int result = tempArr[0];
+    for (int i = 0; i < arrLength; i++) {
+        if(result < tempArr[i]) result = tempArr[i];
+    }
+    return result;
+
+// function returns lowest temp
+}int lowestTemp(const TempArrType tempArr, int arrLength) {
+    int result = tempArr[0];
+    for (int i = 0; i < arrLength; i++) {
+        if(result > tempArr[i]) result = tempArr[i];
+    }
+    return result;
 }
