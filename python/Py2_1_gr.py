49e5b67ad5779651ed52fb562657ee3fa5de0645
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-04-12 17:42:57 -0500

    created and tested functions

---
+++ python/Py2_1_gr.py
@@ -0,0 +1,23 @@
+# This program will demonstrate various ways to
+# use functions in Python.
+# Greg Rodriguez
+# 4/12/2023
+
+# The main function
+def main():
+    welcome_message()  # causes welcome_message to run
+    goodbye_message()
+
+
+# This function is to welcome people to my program
+def welcome_message():
+    print('Welcome to my program using functions')
+    print('My name is Joe Student')
+
+
+def goodbye_message():
+    print("Goodbye")
+
+
+# This is the main function that starts the program in # motion
+main()  # calls main
