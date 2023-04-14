2d670862f68f85ee51098e64850622f0abb40ad7
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-04-14 18:33:12 -0500

    created getPayment function to prompt and validate input from user

--- Project_gr.cpp
+++ Project_gr.cpp
@@ -4,25 +4,55 @@
 
 #include <iostream>
 #include <random>
+#include <iomanip>
 using namespace std;
 
 double getRandomNum(); // generates random number
+int getPayment(); // gets user input for payment
 
 int main() {
-    double randomNum;
+    double randomNum; // holds random number value
+    int payment; // holds payment amount
+
+    cout << fixed << showpoint << setprecision(2); // set decimal point
 
     cout << "*************" << endl;
     cout << "*  Welcome  *" << endl;
     cout << "*************" << endl;
 
     randomNum = getRandomNum();
-    cout << randomNum;
+    cout << "\nAmount to be paid: $" << randomNum << endl << endl;
+
+    payment = getPayment();
+
+    cout << payment << endl;
+
     return 0;
 };
 
+// function uses three classes from the <random> library
 double getRandomNum() {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0.01, 20.00);
     return dis(gen);
+}
+
+// function prints messages and gets user input with validation
+int getPayment() {
+    int payment;
+    bool validInput = false;
+
+    while(!validInput) {
+        cout << "Payment must be 1, 5, 10, or 20" << endl;
+        cout << "Enter payment amount: $" ;
+        cin >> payment;
+
+        if(payment == 1 || payment == 5 ||payment == 10 ||payment == 20) {
+            validInput = true;
+        } else {
+            cout << "Invalid entry, please try again" << endl;
+        }
+    }
+    return payment;
 }
