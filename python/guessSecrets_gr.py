512269e83d62d509d8f130cc25a133048b2410e5
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-04-18 13:57:39 -0500

    completed guess program

--- python/guessSecrets_gr.py
+++ python/guessSecrets_gr.py
@@ -14,7 +14,8 @@ def main():
     # Call getMonth() and assign to variable to birthMonth
     birth_month = get_month()
     # Call correctAnswers(age, weight, birthMonth) and pass data to function
-    print(age, birth_month)
+    correct_answers(age, weight, birth_month)
+    print("Thank you for playing goodbye")
 
 
 # getAge() function will prompt user for input and return the value
@@ -41,13 +42,31 @@ def get_weight():
 
 # getMonth() function will prompt user for input and return the value
 def get_month():
-    return input("Enter the persons birth month: ")
+    while True:
+        month = input("Enter the person's birth month: ")
+        if month.isalpha():
+            return month
+        else:
+            print("Error: Please enter a valid month.")
 
 
 # correctAnswers function will conditionally render the secret answer results
-# if age is 25 print Congratulations, the age is 25 or less
-# if weight is 128 print Congratulations, the weight is 128 or more
-# if birthMonth is April print Congratulations, the  birth month is April
+def correct_answers(age, weight, birth_month):
+    answers_count = 0
+    # if age is 25 print Congratulations, the age is 25 or less
+    if age <= 25:
+        answers_count += 1
+        print("Congratulations, the age is 25 or less")
+    # if weight is 128 print Congratulations, the weight is 128 or more
+    if weight >= 128:
+        answers_count += 1
+        print("Congratulations, the weight is 128 or more")
+    # if birthMonth is April print Congratulations, the  birth month is April
+    if birth_month == "April":
+        answers_count += 1
+        print("Congratulations, the birth month is April")
+    if answers_count == 0:
+        print("No guesses were correct")
 
 
 main()
