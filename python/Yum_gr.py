91c29f5f7ae6e7ea8ca488f9febe90915b033eea
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-04-26 19:37:30 -0500

    added while loop for continuing order, and calculate/print results

--- python/Yum_gr.py
+++ python/Yum_gr.py
@@ -7,18 +7,38 @@
 # Function to print to the user the food and price
 # Function to show user number entries
 # validate the input to ensure only 1,2,3 can be accepted
-# Function to keep set count of user choices in an array
+# Function to keep set and keep count of user choices in an array
+# validate user input and update array with while loop
+# Prompt user to end order or continue with while loop
+# function to calculate pre tax total
+# function to calculate tax
+# print results
 
 def main():
     choice_array = [0, 0, 0]
+    is_continue = True
 
+    # call function to print menu options
     print_menu()
-    user_choice = get_user_input()
-    set_quantity(user_choice, choice_array)
 
-    print(choice_array[0])
-    print(choice_array[1])
-    print(choice_array[2])
+    # Prompt user to end order or continue with while loop
+    while is_continue:
+        user_choice = get_user_input()
+        set_quantity(user_choice, choice_array)
+        answer = input("Do you want to end your order? (yes/no): ")
+        if answer == "no":
+            is_continue = True
+        else:
+            is_continue = False
+
+    # call function to calculate pre tax total
+    pretax_total = float(calc_pretax(choice_array))
+
+    # call function to calculate tax
+    total = float(calc_total(pretax_total))
+
+    # print results
+    print(f"The total price is $: ${total:.2f}")
 
 
 # Function to print menu
@@ -35,7 +55,7 @@ def get_user_input():
             print("Enter 1 for Yum Yum Burger\n"
                   "Enter 2 for Grease Yum Fries\n"
                   "Enter 3 for Soda Yum")
-            user_input = int(input("Enter item choice: "))
+            user_input = int(input("Enter item choice -> "))
             if user_input == 1 or user_input == 2 or user_input == 3:
                 return user_input
             else:
@@ -68,6 +88,20 @@ def set_quantity(user_choice, choice_array):
                 return
         except ValueError:
             print("Error: Please enter a valid number.")
+
+
+# call function to calculate pre tax total
+def calc_pretax(choice_array):
+    pretax_total = (choice_array[0] * .99 +
+                    choice_array[1] * .79 +
+                    choice_array[2] * 1.09)
+    return pretax_total
+
+
+# function to calculate tax
+def calc_total(pretax_total):
+    total = pretax_total + (pretax_total * .06)
+    return total
 
 
 main()
